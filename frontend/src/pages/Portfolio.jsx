0fc7ea1d0cf222181aeb891f26c4f0eb8c896097
Antoine B <110669873+Krxms@users.noreply.github.com>
2023-05-26 21:50:36 +0200

    Update Portfolio.jsx

--- frontend/src/pages/Portfolio.jsx
+++ frontend/src/pages/Portfolio.jsx
@@ -3,7 +3,6 @@ import Vignette from "../components/Vignette";
 
 import "../styles/Portfolio.css";
 import Navbar from "../components/Navbar";
-import CustomCursor from "../components/CustomCursor";
 import VantaFog from "../components/VantaFog";
 
 function Portfolio() {
@@ -11,7 +10,6 @@ function Portfolio() {
     <div className="portfolioPage">
       <VantaFog />
       <Navbar />
-      <CustomCursor />
       <Vignette />
     </div>
   );
