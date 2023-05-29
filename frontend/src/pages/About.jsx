21b7adc8f868072ff5e26b0fac7b2dabfb2a7454
Antoine B <110669873+Krxms@users.noreply.github.com>
2023-05-29 17:55:55 +0200

    ajustement css

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,50 @@
+// import Navbar from "../components/Navbar";
+// import VantaFog from "../components/VantaFog";
+
+// import pp from "../assets/pp.png";
+
+// import "../styles/About.css";
+
+// export default function About() {
+//   return (
+//     <div className="aboutPage">
+//       <VantaFog />
+//       <Navbar />
+//       <div className="background-animation" />
+//       <div className="About">
+//         <div className="aboutTitleContainer">
+//           <img src={pp} alt="profilPicture" className="profilePicture" />
+//           <div className="About-text">
+//             <h1 className="aboutTitle">
+//               <span className="antoineAbout">Antoine</span>&nbsp;Bruneau
+//             </h1>
+//             <h2 className="aboutSubtitle">
+//               EN RECHERCHE D'ALTERNANCE{" "}
+//               <span className="alternance">UI/UX DESIGN</span>
+//             </h2>
+//             <p className="aboutMe">
+//               Créatif et passionné par l'accessibilité, c'est après une première
+//               carrière dans le domaine du spectacle que j'ai choisi de me
+//               reconvertir dans le numérique. <br /> En constant apprentissage,
+//               l'évolution des espaces de vie numériques stimule en moi le désir
+//               d'apporter ma vision au service d'un Web plus accessible et
+//               conscient des enjeux sociaux et écologiques.
+//               <br /> Une première formation dans le développement web m'a permis
+//               de mieux cibler les enjeux et ma place au sein de cet écosystème
+//               et c'est à ce titre que je souhaite me spécialiser dans le domaine
+//               de l'UX/UI Design. Je suis actuellement à la recherche d'une
+//               alternance pour <b>Octobre 2023.</b>
+//             </p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
 import Navbar from "../components/Navbar";
 import VantaFog from "../components/VantaFog";
-
 import pp from "../assets/pp.png";
-
 import "../styles/About.css";
 
 export default function About() {
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,40 +1,9 @@
-import { useState, useEffect } from "react";
 import VantaFog from "../components/VantaFog";
 import NavbarHome from "../components/NavbarHome";
 
 import "../styles/Home.css";
 
 export default function Home() {
-  const [color, setColor] = useState("#219dd3");
-  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 600);
-
-  const changeColor = () =>
-    setColor((prevColor) => (prevColor === "#219dd3" ? "#F7480F" : "#219dd3"));
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsSmallScreen(window.innerWidth <= 600);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (isSmallScreen) {
-      document.addEventListener("click", changeColor);
-    } else {
-      document.removeEventListener("click", changeColor);
-    }
-
-    return () => {
-      document.removeEventListener("click", changeColor);
-    };
-  }, [isSmallScreen]);
-
   return (
     <div className="homePage">
       <VantaFog />
@@ -48,13 +17,7 @@ export default function Home() {
           <div className="line-1">Hi,</div>
           <div className="line-2">my name</div>
           <div className="line-3">
-            is{" "}
-            <span
-              className="antoine"
-              style={{ color: isSmallScreen ? color : undefined }}
-            >
-              Antoine
-            </span>
+            is <span className="antoine">Antoine</span>
           </div>
           <div
             className="line-4-desktop"
